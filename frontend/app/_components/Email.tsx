23224fbfdeb84a19d8b8c20ba04fe5b3c6381714
JayantRautela <jayantrautela0205@gmail.com>
2025-08-23 19:02:31 +0530

    ui/improvements

--- frontend/app/_components/Email.tsx
+++ frontend/app/_components/Email.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { BACKEND_URL } from "@/lib/utils";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 const isEmailValid = (email: string) => {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
@@ -14,7 +13,6 @@ const isEmailValid = (email: string) => {
 
 export function Email({setEmail, setStep, email}: {setEmail: (email: string) => void, setStep: (step: string) => void, email: string}) {   
     const router = useRouter();
-    const [isSendingEmail, setIsSendingEmail] = useState(false);
     return (
         <div className="mx-auto max-h-screen max-w-6xl">
         <div className="absolute top-4 left-4">
@@ -41,13 +39,11 @@ export function Email({setEmail, setStep, email}: {setEmail: (email: string) => 
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
             className="h-14 w-[25rem] text-lg font-semibold text-white"
-            disabled={isSendingEmail}
           />
           <Button
             disabled={!isEmailValid(email)}
             variant="accent"
             onClick={() => {
-                setIsSendingEmail(true);
                 fetch(`${BACKEND_URL}/auth/initiate_signin`, {
                     method: "POST",
                     body: JSON.stringify({ email }),
@@ -64,15 +60,11 @@ export function Email({setEmail, setStep, email}: {setEmail: (email: string) => 
                 }).catch((err) => {
                     console.error(err);
                     toast.error("Failed to send OTP, please retry after a few minutes");
-                }).finally(() => {
-                  setIsSendingEmail(false);
                 });
             }}
             className="h-14 w-[25rem] text-lg font-semibold text-white hover:bg-primary/90"
           >
-            { isSendingEmail ? (
-              <span className="text-muted-foreground">Please Wait...</span>
-            ) : ('Continue with Email')}
+            Continue with Email
           </Button>
           <div className="text-muted-foreground/80 text-sm">
             By continuing, you agree to our{" "}
