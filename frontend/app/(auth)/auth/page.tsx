61a36fcde83d773695dd22a2cb985fe98b192390
rajgupta-ml <pratikrajgupta23@gmail.com>
2025-08-28 11:44:16 +0530

    fix: OTP page change

--- frontend/app/(auth)/auth/page.tsx
+++ frontend/app/(auth)/auth/page.tsx
@@ -5,8 +5,8 @@ import { Email } from "@/app/_components/Email";
 import { Otp } from "@/app/_components/Otp";
 
 function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [step, setStep] = useState("email");
+  const [email, setEmail] = useState("pratikrajgupta23@gmail.com");
+  const [step, setStep] = useState("otp");
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       {step === "email" && <Email setEmail={setEmail} setStep={setStep} email={email} />}
--- frontend/app/_components/Otp.tsx
+++ frontend/app/_components/Otp.tsx
@@ -3,18 +3,23 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import { BACKEND_URL } from "@/lib/utils";
+import { HtmlHTMLAttributes, useRef, useState } from "react";
+import { BACKEND_URL, cn } from "@/lib/utils";
 import { toast } from "sonner";
 
 export function Otp({ email }: { email: string }) {
-  const [otp, setOtp] = useState("");
-
+  // const [otp, setOtp] = useState("");
+  const otp = useRef<string[]> (["", "", "", "", "", ""]);
   const handleLogin = async () => {
     try {
+      const Otp = otp.current.join("");
+      if(Otp.length !== 6) {
+        toast("Invalid OTP")
+        return;
+      }
       const response = await fetch(`${BACKEND_URL}/auth/signin`, {
         method: "POST",
-        body: JSON.stringify({ email, otp }),
+        body: JSON.stringify({ email, otp  :Otp }),
         headers: {
           "Content-Type": "application/json",
         },
@@ -52,16 +57,54 @@ export function Otp({ email }: { email: string }) {
             </Link>
           </Button> */}
       </div>
-      <div className="flex h-full flex-col items-center justify-center gap-8">
-        <div className="flex flex-col items-center gap-2">
+      <div className="flex h-full flex-col items-center justify-center gap-8 shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-20">
+        <div className="flex flex-col items-center gap-2 mb-2">
           <h1 className="text-3xl lg:text-4xl font-bold tracking-tighter">
             Welcome to <span className="text-primary">1ai</span>
           </h1>
+            <span>Enter the verification code sent to your email</span>
         </div>
         <div className="flex flex-col gap-2 w-full">
-          <Input disabled placeholder="Email" value={email} />
+          <div className="bg-muted/20 p-2 rounded-lg flex items-center">{email} </div>
 
-          <Input placeholder="OTP" onChange={(e) => setOtp(e.target.value)} />
+          <span>Enter Your OTP</span>
+          <div className="flex items-center">
+            {([...Array(6).keys()]).map((_, index) => (
+
+              <Input
+                key={index}
+                inputMode="numeric"
+                maxLength={1}
+                pattern="[0-9]"
+                className={cn(
+                  "w-[40px] rounded-none text-center mb-2",
+                  index === 0 && "rounded-l-xl",
+                  index === 5 && "rounded-r-xl"
+                )}
+                
+                onInput={(e : React.ChangeEvent<HTMLInputElement>) => {
+                  const input =  e.target
+                  input.value = input.value.replace(/[^0-9]/g, "");
+                  const nextSibling = input.nextSibling as HTMLInputElement
+                  otp.current[index] = input.value;
+                  console.log(otp.current)
+                  if(nextSibling && input.value.match("[0-9]")){
+                    nextSibling.focus()
+                  }
+                }}
+
+                onKeyDown={(e : React.KeyboardEvent<HTMLInputElement>) => {
+                  const input = e.currentTarget;
+                  if (e.key === "Backspace") {
+                    if (!input.value && input.previousElementSibling) {
+                      (input.previousElementSibling as HTMLInputElement).focus();
+                    }
+                  }
+                }}
+              />
+            ))}
+          </div>
+
           <Button
             variant="accent"
             onClick={handleLogin}
@@ -72,11 +115,11 @@ export function Otp({ email }: { email: string }) {
         </div>
         <div className="text-muted-foreground text-sm">
           By continuing, you agree to our{" "}
-          <Link href="/terms" className="text-muted-foreground font-medium">
+          <Link href="/terms" className="text-primary  font-medium">
             Terms of Service
           </Link>{" "}
           and{" "}
-          <Link href="/privacy" className="text-muted-foreground font-medium">
+          <Link href="/privacy" className="text-primary font-medium">
             Privacy Policy
           </Link>
         </div>
